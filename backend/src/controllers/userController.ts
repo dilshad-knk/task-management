13a9f64ec081103ef63db3abee036cc9c88d29e6
dilshad <dilshadali.knk@gmail.com>
2024-08-25 14:01:20 +0530

    cookies settings changes

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -5,72 +5,78 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
 
 
-export const createUser = async (req:Request,res:Response) => {
-    
-    const {email,password} = req.body;
+export const createUser = async (req: Request, res: Response) => {
 
-   
-  
+  const { email, password } = req.body;
 
-    try {
 
-        const existingUser = await User.findOne({ email });
 
-        if (existingUser) {
-            return res.status(400).json({ error: 'User already exists' });
-          }
-        
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const userName = email.split('@')[0]
 
-        const user = new User({email,password:hashedPassword,userName}); 
-        await user.save();
-       
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-        
-        res.cookie('token',token)
+  try {
 
-        res.status(201).json({message : 'user registered'});
+    const existingUser = await User.findOne({ email });
 
-    } catch (error) {
-        res.status(500).json({message : 'failed to register user',error});
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userName = email.split('@')[0]
+
+    const user = new User({ email, password: hashedPassword, userName });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+    res.cookie('token', token)
+
+    res.status(201).json({ message: 'user registered' });
+
+  } catch (error) {
+    res.status(500).json({ message: 'failed to register user', error });
+  }
 
 
 }
 
-export const login = async (req:Request,res:Response)=>{
-    try {
-        const {email,password} = req.body;
-        const user = await User.findOne({email})
-        
-
-        if(!user){
-            return res.status(404).json({message:"User not found"})
-        }
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
 
 
-        const isValid = await bcrypt.compare(password,user.password)
-
-        if(!isValid) {
-            return res.status(401).json({message:'Invalid credent'})
-        }
-
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-
-
-    
-        res.cookie('token',token)
-
-        const { password: _, ...userObject } = user.toObject();
-      
-
-        res.status(200).json({message : 'access granted',user: userObject ,token});
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error: 'Internal server error'})
+    if (!user) {
+      return res.status(404).json({ message: "User not found" })
     }
+
+
+    const isValid = await bcrypt.compare(password, user.password)
+
+    if (!isValid) {
+      return res.status(401).json({ message: 'Invalid credent' })
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+    const cookieParams = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      path: '/', // Adjust as needed
+
+    };
+
+    res.cookie('token', token, cookieParams)
+
+    const { password: _, ...userObject } = user.toObject();
+
+
+    res.status(200).json({ message: 'access granted', user: userObject, token });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Internal server error' })
+  }
 }
 
 
--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -3,40 +3,39 @@ import jwt from 'jsonwebtoken';
 
 
 export interface Request1 extends Request {
-    userId? : string
+  userId?: string
 }
 
 
-export const verify = (req:Request1,res:Response,next:NextFunction) => {
+export const verify = (req: Request1, res: Response, next: NextFunction) => {
 
-    const token = req.cookies?.token;
-
-   
-    console.log('authorhyytized')
-    
-  
-    jwt.verify(token,process.env.JWT_SECRET as string,(err:any,decode:any) => {
-        if (err){
-            return  res.status(401).json({
-                success : false,
-                message : "Unauthorized!!",
-                err
-            })
-
-        }
-        
-         const userId = decode.userId;
+  const token = req.cookies?.token;
 
 
 
-         
-         req.userId = userId; 
 
-       
+  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, decode: any) => {
+    if (err) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized!!",
+        err
+      })
 
-        next()
-        
+    }
 
-    })
+    const userId = decode.userId;
+
+
+
+
+    req.userId = userId;
+
+
+
+    next()
+
+
+  })
 
 }
